9da7c2c67d3ebf384c59264ca545e10931fce1ad
sunlijiang <sunlijiang@mokahr.com>
2019-07-27 13:09:58 +0800

    refactor

--- src/test/java/com/lieve/base/nio/Client.java
+++ src/test/java/com/lieve/base/nio/Client.java
@@ -11,6 +11,7 @@ import static com.lieve.base.nio.Server.send;
 import static java.util.Objects.nonNull;
 
 /**
+ * TODO 非阻塞模式与选择器搭配会工作的更好，通过将一或多个SocketChannel注册到Selector，可以询问选择器哪个通道已经准备好了读取，写入等
  * @author sunlijiang
  * @date 2019/7/26
  */
@@ -18,7 +19,14 @@ public class Client {
     public static void main(String[] args) {
         try {
             SocketChannel socketChannel = SocketChannel.open();
-            socketChannel.connect(new InetSocketAddress("localhost", PORT));
+            socketChannel.connect(new InetSocketAddress(PORT));
+
+            /**
+             * 可以设置 @see SocketChannel 为非阻塞模式（non-blocking mode)。
+             * 设置之后，就可以在异步模式下调用connect(),read()和write()了。
+             * SocketChannel在非阻塞模式下，此时调用connect()，该方法可能在连接建立之前就返回了。
+             * 为了确定连接是否建立，可以调用finishConnect()的方法。
+             */
             socketChannel.configureBlocking(false);
             while (socketChannel.finishConnect()) {
                 send(socketChannel, "who are you?");
@@ -50,18 +58,32 @@ public class Client {
      * @param channel
      * @return
      */
-    public static String receiver(SocketChannel channel) {
+    public static String receiver(SocketChannel channel) throws IOException {
         ByteBuffer buffer = ByteBuffer.allocate(BUFFER_CAPACITY);
 
         StringBuilder result = new StringBuilder();
         if (nonNull(channel)) {
-            int bytesRead = 0;
+            int bytesRead = -1;
             try {
+                /**
+                 * 非阻塞模式下,read()方法在尚未读取到任何数据时可能就返回了。
+                 * 所以需要关注它的int返回值，它会告诉你读取了多少字节。
+                 */
                 bytesRead = channel.read(buffer);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            while (bytesRead != -1) {
+
+            /**
+             *
+             */
+            if (bytesRead == -1) {
+                System.out.println("Close channel : " + channel.getRemoteAddress());
+                channel.close();
+                System.exit(0);
+            }
+
+            while (bytesRead > 0) {
                 System.out.println("read : " + bytesRead);
                 /**
                  * 注意 buf.flip() 的调用,首先读取数据到Buffer,
@@ -80,12 +102,6 @@ public class Client {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-
-            }
-            try {
-                channel.close();
-            } catch (IOException e) {
-                e.printStackTrace();
             }
         }
         return result.toString();
--- src/test/java/com/lieve/base/nio/Server.java
+++ src/test/java/com/lieve/base/nio/Server.java
@@ -4,8 +4,9 @@ package com.lieve.base.nio;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
+import java.nio.channels.*;
+import java.util.Iterator;
+import java.util.Set;
 
 import static com.lieve.base.nio.Client.receiver;
 import static com.lieve.base.nio.NioConstants.BUFFER_CAPACITY;
@@ -20,14 +21,68 @@ public class Server {
 
     public static void main(String[] args) {
         try {
+            /**
+             * 通过 ServerSocketChannel.accept() 方法监听新进来的连接。
+             * 当 accept()方法返回的时候,它返回一个包含新进来的连接的 SocketChannel。因此, accept()方法会一直阻塞到有新连接到达。
+             *
+             * 通常不会仅仅只监听一个连接,在while循环中调用 accept()方法
+             */
             ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+            // serverSocketChannel.socket().bind(new InetSocketAddress(PORT));
             serverSocketChannel.bind(new InetSocketAddress(PORT));
             serverSocketChannel.configureBlocking(false);
-
+            Selector selector = Selector.open();
+            SelectionKey selectionKey = serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
             while (true) {
-                SocketChannel socketChannel = serverSocketChannel.accept();
-                send(socketChannel, "who are you?");
-                System.out.println(receiver(socketChannel));
+                /**
+                 * ServerSocketChannel可以设置成非阻塞模式。在非阻塞模式下，
+                 * accept() 方法会立刻返回，如果还没有新进来的连接,返回的将是null。
+                 * 因此，需要检查返回的SocketChannel是否是null
+                 */
+                int readyChannels = selector.select();
+                if (readyChannels == 0) {
+                    continue;
+                }
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                Iterator<SelectionKey> keyIterator = selectionKeys.iterator();
+                while(keyIterator.hasNext()) {
+                    SelectionKey key = keyIterator.next();
+//                    SocketChannel socketChannel = null;
+                    if (key.isAcceptable()) {
+                        register(selector, serverSocketChannel);
+//                        key.attachment()
+//                        socketChannel = serverSocketChannel.accept();
+//                        socketChannel.configureBlocking(false);
+                        System.out.println("a connection was accepted by a ServerSocketChannel.");
+//                        send(channel, "who are you?");
+                    } else if (key.isConnectable()) {
+                        System.out.println("a connection was established with a remote server.");
+                    } else if (key.isReadable()) {
+                        SocketChannel channel = (SocketChannel) key.channel();
+                        ByteBuffer buffer = (ByteBuffer) key.attachment();
+                        long bytesRead = channel.read(buffer);
+                        if (bytesRead == -1) {
+                            channel.close();
+                        } else if (bytesRead > 0) {
+                            key.interestOps(SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+                            System.out.println("Get data length : " + bytesRead);
+                        }
+                        // System.out.println(receiver(channel));
+                        System.out.println("a channel is ready for reading");
+                    } else if (key.isValid() && key.isWritable()) {
+                        ByteBuffer buffer = (ByteBuffer)key.attachment();
+                        buffer.flip();
+                        SocketChannel channel = (SocketChannel) key.channel();
+                        channel.write(buffer);
+                        if (!buffer.hasRemaining()) {
+                            key.interestOps(SelectionKey.OP_READ);
+                        }
+                        buffer.compact();
+                        // send(channel, "who are you?");
+                    }
+                    keyIterator.remove();
+                }
+
                 /*ByteBuffer byteBuffer = ByteBuffer.allocate(BUFFER_CAPACITY);
                 byteBuffer.clear();
                 if (nonNull(socketChannel)) {
@@ -49,6 +104,18 @@ public class Server {
     }
 
     /**
+     * 绑定SocketChannel 到selector
+     * @param selector
+     * @param serverSocketChannel
+     * @throws IOException
+     */
+    public static void register(Selector selector, ServerSocketChannel serverSocketChannel) throws IOException {
+        SocketChannel socketChannel = serverSocketChannel.accept();
+        socketChannel.configureBlocking(false);
+        socketChannel.register(selector, SelectionKey.OP_READ);
+    }
+
+    /**
      * send message by channel
      * 写数据到SocketChannel, 使用channel.write(byteBuffer)
      * @param channel
@@ -62,6 +129,7 @@ public class Server {
             byteBuffer.flip();
 
             /**
+             * 非阻塞模式下，write()方法在尚未写出任何内容时可能就返回了。所以需要在循环中调用write()。
              * write()方法的调用是在一个while循环中的。
              * write()方法无法保证能写多少字节到SocketChannel。
              * 所以我们重复调用write()直到Buffer没有要写的字节为止。
